061d60a40fe1b9e6d8e2a34d70d4ab03a70ab937
theresamay <theresa.may.001@gmail.com>
2016-09-01 21:30:33 -0700

    didn't want levels for days

--- Import_Data.R
+++ Import_Data.R
@@ -11,5 +11,7 @@ library(reshape2)
 library(lubridate)
 
 # assemble the main datafile
-Pass_Reset <- read.csv("Data/SD_Password_Reset.csv", header=TRUE, sep = ",", stringsAsFactors = FALSE)
+Pass_Reset <- read.csv("Data/SD_Password_Reset.csv", header=TRUE, 
+                       sep = ",", stringsAsFactors = FALSE, 
+                       as.is = c("DAY"))
 # end
