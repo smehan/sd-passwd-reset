253e1319df212aaddd571c8afcb995dfbd1a10f7
theresamay <theresa.may.001@gmail.com>
2016-09-01 21:23:29 -0700

    loaded the initial data set.  data loaded to object.

---
+++ Import_Data.R
@@ -0,0 +1,15 @@
+###########################################################
+### Importing and cleansing Password Reset Data
+###########################################################
+library(ggplot2)
+library(scales)
+library(ggthemes)
+library(plyr)
+library(dplyr)
+library(stringr)
+library(reshape2)
+library(lubridate)
+
+# assemble the main datafile
+Pass_Reset <- read.csv("Data/SD_Password_Reset.csv", header=TRUE, sep = ",", stringsAsFactors = FALSE
+# end
