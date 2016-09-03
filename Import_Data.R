4fc6d86c1915a3be5da7143ebf4b5861431b9475
Shawn Mehan <shawn.mehan@ieee.org>
2016-09-03 14:23:53 -0700

    typo from global replace

--- Import_Data.R
+++ Import_Data.R
@@ -11,7 +11,7 @@ library(reshape2)
 library(lubridate)
 
 # assemble the main datafile
-pass_reset_df_df <- read.csv("Data/SD_Password_Reset.csv", header=TRUE,
+pass_reset_df <- read.csv("Data/SD_Password_Reset.csv", header=TRUE,
                        sep = ",", stringsAsFactors = FALSE,
                        as.is = c("DAY"))
 # end
