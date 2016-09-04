f7359c2bb56ffbc102af99567b9674c3caa9e73e
Shawn Mehan <shawn.mehan@ieee.org>
2016-09-03 17:32:54 -0700

    Understood the frequency at last. Remember the periodicity function in xts if you want to estimate the data's periodicity. This is daily, so frequency is 365. ts() is NOT aggregating counts with the frequency func, so it would need to be done manually if needed.

--- Time_Series.R
+++ Time_Series.R
@@ -17,54 +17,50 @@ pass_reset_df <- readRDS("data/pass_reset_df.RDS")
 self_change_xts <- xts(pass_reset_df$SELF_PASSWORD_CHANGE, order.by = pass_reset_df$DAY)
 
 plot(pass_reset_df$SELF_PASSWORD_CHANGE, main="Self-Password Changes Over Time", pch=1, col="blue")
+plot(self_change_xts, main="Self-Password Changes Over Time", pch=1, col="green")
+
+# Plot time-series as a curve, for all time.
+plot.ts(self_change_xts, main="Self Password Changes per day\nFrom 2012-07",
+        xlab="Day", ylab="Count", col="blue")
 
 
 # Plot time-series as a curve, starting from 07/2012 onwards.
 plot.ts(self_change_xts['2012-07/'], main="Self Password Changes per day\nFrom 2012-07",
-     xlab="Date", ylab="Count", col="green")
-
-
-# Plot time-series as a curve, for all time.
-plot.ts(self_change_xts, main="Self Password Changes per day\nFrom 2012-07",
-        xlab="Date", ylab="Count", col="blue")
+     xlab="Day", ylab="Count", col="green")
 
 
 # Plot time-series curve for the month of August, 2014.
-plot.ts(self_change_xts['2014-08'], main="Self Password Changes per day\nFrom 2012-07",
-        xlab="Date", ylab="Count", col="red")
+plot.ts(self_change_xts['2014-08'], main="Self Password Changes per day\nin Aug 2014",
+        xlab="Day", ylab="Count", col="red")
 
 
 ##########################################################
 # Time series need to understand the frequency of the TS.
 # Here we are using another TS library that is building on the XTS df already built.
 
-self_day <- ts(self_change_xts, frequency=365)
-self_week <- ts(self_change_xts, frequency=52)
-self_month <- ts(self_change_xts, frequency=12)
-self_quarterly <- ts(self_change_xts, frequency=4)
-self_annual <- ts(self_change_xts, frequency=1)
+self_change_ts <- ts(self_change_xts, frequency=365)
 
 # now we decompose it into its various parts from which we can analyze and plot
-self_day_components <- decompose(self_day)
-self_week_components <- decompose(self_week)
-self_month_components <- decompose(self_month)
-self_quarterly_components <- decompose(self_quarterly)
-self_annual_components <- decompose(self_annual)
+self_change_components <- decompose(self_change_ts)
 
-plot(self_day_components, col="blue")
-plot(self_week_components, col="blue")
-plot(self_month_components, col="red")
-plot(self_quarterly_components, col="purple")
+plot(self_change_components, col="blue")
 
 # another way to do the decompositions is with stl
 # Seasonal decomposition
-fit <- stl(ts(as.numeric(self_change_xts), frequency=12), s.window="periodic", robust=TRUE)
+fit <- stl(ts(as.numeric(self_change_xts), frequency=365), s.window="periodic", robust=TRUE)
 plot(fit)
 
-monthplot(ts(as.numeric(self_change_xts), frequency=12))
 
+# These appear to average everything into a single year (probably from the frequency?)
+# and show by days in the year.
+monthplot(ts(as.numeric(self_change_xts), frequency=365))
+monthplot(fit, choice="seasonal")
+monthplot(fit, choice="trend")
+
+# this is using a different kind of model fitting.
+# presumably it allows for multiplicative as well as additive
+# the spikey nature of the data might be damaging the model fit
 fit2 <- ets(ts(as.numeric(self_change_xts), frequency=365))
-
 plot(fit2)
 
 # method to estimate statiscal significance of seasonal component
@@ -75,4 +71,21 @@ plot(fit2)
 # df <- attributes(logLik(fit1))$df - attributes(logLik(fit2))$df
 # 1-pchisq(deviance,df)
 
-log(ts(as.numeric(self_change_xts), frequency=365))
+
+# this is less cooked, and looking at logs of the data as well as
+# doing differentials, i.e., from day to day.
+# http://www.statmethods.net/advstats/timeseries.html
+self_change_log <- log(ts(as.numeric(self_change_xts), frequency=365))
+plot(self_change_log)
+plot(diff(self_change_log))
+plot(diff(self_change_xts))
+
+# let's look at a histogram of the counts, and make the bins a bit smaller.
+hist(self_change_ts, breaks = 100)
+
+# definitely pareto (power-law) distribution, so let's confirm with a qqplot
+qqnorm(diff(self_change_ts))
+
+# http://www.statoek.wiso.uni-goettingen.de/veranstaltungen/zeitreihen/sommer03/ts_r_intro.pdf
+
+
